c4f2b648225e86c8a7bd8e0b9e43d39a8b15bc92
Sahan <shnrndk@gmail.com>
2021-03-09 18:13:12 +0530

    db connection

---
+++ index.js
@@ -0,0 +1,46 @@
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const consola = require('consola');
+const { DB, PORT } = require('./config')
+
+app.use(cors);
+app.use(bodyParser.json());
+
+const startApp = async () => {
+    try {
+        await mongoose.connect(DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+
+        consola.success({
+            message: `Successfully connected to DB \n${DB}`,
+            badge: true
+        })
+
+        app.listen(PORT, () => {
+            consola.success({
+                message: `Example app listening at http://localhost:${PORT}`,
+                badge: true
+            })
+        })
+
+    } catch (err) {
+        consola.error({
+            message: `Unable to connect to DB \n${err}`,
+            badge: true
+        })
+        startApp()
+    }
+};
+
+startApp();
+
+
+
+
